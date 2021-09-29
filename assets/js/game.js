4760d5818e0e07a8a8b5333e32f5a6df333f0287
School <kaboy@icloud.com>
2021-09-29 01:48:25 -0500

    add functions and variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function name "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// fight();
