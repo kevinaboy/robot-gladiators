291ef8440aed438cf781abfd58c1a2618e7c85a5
School <kaboy@icloud.com>
2021-10-14 00:05:33 -0500

    add prompt for player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,6 @@ var startGame = function () {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-      debugger;
 
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
@@ -170,9 +169,23 @@ var shop = function () {
 
 /* GAME INFORMATION / VARIABLES */
 
+// function to set name
+var getPlayerName = function () {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  // ADD LOOP HERE WITH PROMPT AND CONDITION
+  // ***************************************
+
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
